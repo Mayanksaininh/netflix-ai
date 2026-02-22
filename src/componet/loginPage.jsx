47a42ef649c45076a7f64d2fcb45c89e82100665
Mayanksaininh <mayanksaininh@gmail.com>
2026-02-22 20:23:55 +0530

    Add Sign Up feature using usestate react hook

--- src/componet/loginPage.jsx
+++ src/componet/loginPage.jsx
@@ -1,14 +1,39 @@
+import { useState } from "react"
+
 const LoginPage = ()=> {
+ const [IsSignInform,setIsSignInform] = useState(true)
+
+
+  const toggleSignInform = () =>{
+   setIsSignInform(!IsSignInform)
+  }
   return (
     <div className="">
       <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
         <div className="sm:mx-auto sm:w-full sm:max-w-sm">
          
-          <h2 className="mt-10 text-center text-2xl/9 font-bold tracking-tight text-white">Sign in to your account</h2>
+          <h2 className="mt-10 text-center text-2xl/9 font-bold tracking-tight text-white">{IsSignInform ? "Sign In Your account" : "Sign Up Your account"}</h2>
         </div>
 
         <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
           <form action="#" method="POST" className="space-y-6">
+             {!IsSignInform  &&  (
+              <div>
+              <label htmlFor="Name" className="block text-sm/6 font-medium text-gray-100">
+                Full Name
+              </label>
+              <div className="mt-2">
+                <input
+                  id=" Full Name"
+                  name="Name"
+                  type="Name"
+                  required
+                  autoComplete
+                  className="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-white outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6"
+                />
+              </div>
+            </div> 
+          ) }
             <div>
               <label htmlFor="email" className="block text-sm/6 font-medium text-gray-100">
                 Email address
@@ -41,6 +66,7 @@ const LoginPage = ()=> {
                   autoComplete="current-password"
                   className="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-white outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6"
                 />
+                
               </div>
             </div>
 
@@ -49,9 +75,10 @@ const LoginPage = ()=> {
                 type="submit"
                 className="flex w-full justify-center rounded-md bg-indigo-500 px-3 py-1.5 text-sm/6 font-semibold text-white hover:bg-indigo-400 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
               >
-                Sign in
+                {IsSignInform ? "Sign in" : "Sign Up "}
               </button>
             </div>
+           <p className="text-center tracking-tight text-white cursor-pointer" onClick={toggleSignInform}> {IsSignInform ? "New User? Sign Up Now..." : "Already user, Sign In Now..."}</p>
           </form>
         </div>
       </div>
