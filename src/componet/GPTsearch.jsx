da3354c82890b98a62b6710365d60251c2992df2
Mayanksaininh <mayanksaininh@gmail.com>
2026-03-30 01:18:24 +0530

    Make Background of ASk AI page

--- src/componet/GPTsearch.jsx
+++ src/componet/GPTsearch.jsx
@@ -1,9 +1,17 @@
 import GPTMovieSugg from "./GPTmovieSugg"
 import SearchBar from "./GPTsearchbar"
+import { BacKGround_Image } from "../utils/Constant";
+
 
 const GPTsearch= () =>{
    return (
-    <div> 
+    <div>
+       <img
+              src={BacKGround_Image}
+              alt="Background"
+              className="absolute inset-0 w-full h-full object-cover -z-10"
+      /> 
+      
       <SearchBar/>
       <GPTMovieSugg/>
     </div>
